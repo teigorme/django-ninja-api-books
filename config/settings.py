3478d1abea50d6085dcdfe8178b4d6143106c2fc
teigorme <igorteixeira354@gmail.com>
2025-10-23 21:21:20 +0100

    feat: add script for run server

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yeyb^84h)+xh1o_f&y*85*ef^8sxgey^5h&ym73l^m_k)+#nlc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
